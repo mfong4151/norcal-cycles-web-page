943339f0a2a0132a925d41ab3bf90bf3420dd48e
Max <mfong415@gmail.com>
2023-08-13 14:03:29 -0700

    Various styling edits and copy formatting

--- src/components/ContactUs/index.jsx
+++ src/components/ContactUs/index.jsx
@@ -3,8 +3,25 @@ import './ContactUs.css'
 
 const ContactUs = () => {
   return (
-    <div>
+    <div className='udc-left fdc'>
+      <h1>Contact Us</h1>
+      <ul className='udc-left fdc'>
+        <li>Phone: 650-872-2354</li>
+        <li>General Questions - Info@norcalcycles.com</li>
+        <li>Sales Department - Sales@norcalcycles.com</li>
+        <li>Service Department - Service@norcalcycles.com</li>
+        <li>Used Parts Department - Parts@norcalcycles.com</li>
+        <li>Address - 1585-1587 El Camino Real Millbrae, CA 94030</li>
+      </ul>
       
+      <h1>Our Hours</h1>
+        <ul className='udc-left fdc'>
+          <li>Tuesday - Friday: 10am to 7pm</li>
+          <li>Saturday: 10am to 5pm</li>
+          <li>Closed for Lunch: 12pm to 1pm</li>
+          <li>Closed Sundays & Mondays</li>
+          <li>Closed All Holidays </li>
+        </ul>
     </div>
   )
 }
--- src/components/Rentals/index.jsx
+++ src/components/Rentals/index.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Rentals = () => {
   return (
-    <div>
-      
+    <div className='udc fdc'>
+      <h1>Rentals</h1>
+      <div>
+        <p>Nor-Cal Cycles offers a variety of motorcycles for rent.</p>
+        <p>Visit our <a href='https://rentalpowersports.com/'>rental store </a></p>
+        <p>Contact us for more info - Let's have you riding today!</p>
+      </div>
     </div>
   )
 }
