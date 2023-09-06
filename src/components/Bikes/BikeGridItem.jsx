10180674424934e4a833075e5683b1b00346ef7e
Max <mfong415@gmail.com>
2023-09-05 17:30:07 -0700

    Added mobile formating to bikes grid

--- src/components/Bikes/BikeGridItem.jsx
+++ src/components/Bikes/BikeGridItem.jsx
@@ -14,17 +14,20 @@ const BikeGridItem = ({bike}) => {
   }
   
   return (
-    <div className='bike se cursor-events' onClick={handleOnClick}>
-        <img className='bike-img' src={bike.images[0]} alt="" />
-        <div className='info-holder'>
-            <h4 className="name">{bikeName}</h4>
-            <ul className='fdc'>
-                <li className="transmission">{`Transmission: ${bike.transmission}`}</li>
-                <li className="engine">{`Engine: ${bike.engine}`}</li>
-                <li className="milelage">{`Miles: ${bike.miles}`}</li>
-            </ul>
-            <p className='bike-id'>{`Stock No: ${bike.id}`}</p>
-            <p className="price">{`$${bike.price}`}</p>
+    <div className='bike cursor-events' onClick={handleOnClick}>
+        <h4 className="name udc">{bikeName}</h4>
+        <div className='bike-itm-body'>
+          <img className='bike-img' src={bike.images[0]} alt=""/>
+          <div className='info-holder'>
+              <ul className='fdc'>
+                  <li className="transmission">{`Transmission: ${bike.transmission}`}</li>
+                  <li className="engine">{`Engine: ${bike.engine}`}</li>
+                  <li className="milelage">{`Miles: ${bike.miles}`}</li>
+              </ul>
+              <p className='bike-id'>{`Stock No: ${bike.id}`}</p>
+              <p className="price">{`$${bike.price}`}</p>
+          </div>
+
         </div>
     </div>
   )
